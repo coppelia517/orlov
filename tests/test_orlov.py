ba88a7b036ecde33b6506352df106b9cccb8695a
edith coppelia <dev.coppelia@gmail.com>
2018-07-30 21:48:22 +0900

    update.

--- tests/test_orlov.py
+++ tests/test_orlov.py
@@ -1,22 +1,22 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 """Tests for `orlov` package."""
+import pytest
+import logging
+
+logger = logging.getLogger(__name__)
 
 
-import unittest
-
-from orlov import orlov
-
-
-class TestOrlov(unittest.TestCase):
+@pytest.mark.usefixtures('conftests_fixture', 'testcase_fixture')
+class TestOrlov:
     """Tests for `orlov` package."""
 
-    def setUp(self):
-        """Set up test fixtures, if any."""
-
-    def tearDown(self):
-        """Tear down test fixtures, if any."""
+    @classmethod
+    @pytest.fixture(scope='function')
+    def testcase_fixture(cls):
+        """ fixture executed once for the test suite """
+        logger.info('Setup the test case')
+        yield
+        logger.info('Tearing down the test case')
 
     def test_000_something(self):
         """Test something."""
+        logger.info('test')
