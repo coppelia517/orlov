e5e8431f75bc89cca98ee29e19b613be2a0338ed
edith coppelia <dev.coppelia@gmail.com>
2018-08-20 20:08:21 +0900

    update.

--- orlov/plugins.py
+++ orlov/plugins.py
@@ -1,6 +1,7 @@
 """ Orlov Plugin Module. """
 import inspect
 import logging
+import pytest
 
 from orlov.libs.adb.fixture import android
 from orlov.libs.workspace.fixture import workspace
@@ -41,5 +42,43 @@ def pytest_runtest_call(item):
     logger.debug(inspect.currentframe().f_code.co_name)
 
 
-def pytest_runtest_teardown(item, nextitem):
-    logger.debug(inspect.currentframe().f_code.co_name)
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item):
+    """
+    Pytest hookwrapper for makereport.
+    This hook executes once for each test phase (setup, call, teardown).
+    We can store the result of each phase in this hook. This is done so pytest_runtest_teardown hook can read it.
+    :param item: Pytest test item
+    """
+    logger.debug("Setup of pytest_runtest_makereport")
+    outcome = yield
+    logger.debug("Teardown of pytest_runtest_makereport")
+    # outcome.excinfo may be None or a (cls, val, tb) tuple
+
+    res = outcome.get_result()  # will raise if outcome was exception
+    # Pass the slaveinfo to report
+    res.slaveinput = getattr(item.config, "slaveinput", None)
+    # Store the result of each test phase, so it can be read by pytest_runtest_teardown hook when it runs.
+    if not hasattr(item, "rep_" + res.when):
+        setattr(item, "rep_" + res.when, res)
+
+
+def pytest_runtest_teardown(item):
+    """
+    Pytest hook for capturing screenshot on test failure.
+    After each test phase execution is done, this hook is called.
+    This is run before any other fixtures are called.
+    :param item:
+    :param nextitem:
+    :return:
+    """
+    logger.debug("Begin pytest_runtest_teardown")
+
+    if (hasattr(item, 'rep_setup') and item.rep_setup.failed) or (hasattr(item, 'rep_call') and item.rep_call.failed):
+        filename = "errorshot_{}_{}.png".format(item.name, time.strftime("%Y_%m_%d_%H_%M_%S"))
+        if hasattr(item.cls, "result_dir"):
+            filename = os.path.join(item.cls.result_dir, filename)
+            item.cls.driver.save_screenshot(filename)
+            logger.debug("Screenshot Captured on Test Failure.")
+        else:
+            logger.info("result_dir does not exist, screen shot not saved.")
